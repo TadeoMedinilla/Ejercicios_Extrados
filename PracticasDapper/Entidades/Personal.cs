8b9fe076cb92fcf97239132d7c543298b5a4528b
TadeoMedinilla <108036341+TadeoMedinilla@users.noreply.github.com>
2022-12-16 10:34:27 -0300

    Add files via upload

---
+++ PracticasDapper/Entidades/Personal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticasDapper.Entidades
+{
+    class Personal
+    {
+        public int ID_Empleado { get; set; }
+        public string  Nombre { get; set; }
+        public string Puesto { get; set; }
+        public int ID_Jefe { get; set; }
+
+    }
+    class Empleado:Personal
+    { 
+    }
+
+    class Supervisor: Personal
+    {
+        public int Empleados_Supervisados { get; set; }
+    }
+    
+    class Encargado: Personal
+    {
+        public int Sucursales_Supervisadas { get; set; }
+    }
+
+}
